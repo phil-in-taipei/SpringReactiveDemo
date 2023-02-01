6bf0bb66f1034277273ba0ed5bb7599d23d497b1
mikalikahi <sweeney.phil@gmail.com>
2023-02-01 15:54:05 +0800

    Other controller methods for students, which are then implemented in the RouterConfig

--- src/main/java/SpringReactiveDemo/SpringReactiveDemo/config/RouteConfig.java
+++ src/main/java/SpringReactiveDemo/SpringReactiveDemo/config/RouteConfig.java
@@ -1,7 +1,36 @@
 package SpringReactiveDemo.SpringReactiveDemo.config;
 
+import SpringReactiveDemo.SpringReactiveDemo.controllers.StudentController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Configuration
 public class RouteConfig {
+
+    @Bean
+    RouterFunction<ServerResponse> routes(StudentController studentController) {
+        return route(
+                GET("/students")
+                        .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), studentController::getAllStudents)
+                .andRoute(GET("/students/{id}")
+                        .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), studentController::getStudent)
+                .andRoute(POST("/students")
+                        .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), studentController::addNewStudent)
+                .andRoute(PUT("/students/{id}")
+                        .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), studentController::updateStudent)
+                .andRoute(DELETE("/students/{id}")
+                        .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), studentController::deleteStudent);
+    }
 }
--- src/main/java/SpringReactiveDemo/SpringReactiveDemo/controllers/StudentController.java
+++ src/main/java/SpringReactiveDemo/SpringReactiveDemo/controllers/StudentController.java
@@ -8,7 +8,10 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.http.HttpStatus;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
@@ -44,7 +47,46 @@ public class StudentController {
                 });
     }
 
+    public Mono<ServerResponse> getAllStudents(ServerRequest request) {
+        Flux<Student> students = studentService.getAllStudents();
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(students, Student.class);
+    }
 
+    public Mono<ServerResponse> getStudent(ServerRequest request) {
+        Integer studentId = Integer.parseInt(request.pathVariable("id"));
+        return studentService.getStudent(studentId)
+                .flatMap(student -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(Mono.just(student), Student.class))
+                .switchIfEmpty(ServerResponse.notFound()
+                        .build());
+    }
 
+    public Mono<ServerResponse> addNewStudent(ServerRequest request) {
+        Mono<Student> studentMono = request.bodyToMono(Student.class);
+        Mono<Student> newStudent = studentMono.flatMap(studentService::saveStudent);
+        return ServerResponse.status(HttpStatus.CREATED)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(newStudent, Student.class);
+    }
+
+    public Mono<ServerResponse> updateStudent(ServerRequest request) {
+        String studentId = request.pathVariable("id");
+        Mono<Student> studentMono = request.bodyToMono(Student.class);
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(studentService.updateStudent(Integer.valueOf(studentId), studentMono), Student.class);
+    }
+
+    public Mono<ServerResponse> deleteStudent(ServerRequest request) {
+        String studentId = request.pathVariable("id");
+        return studentService.getStudent(Integer.valueOf(studentId))
+                .flatMap(student -> studentService
+                        .deleteStudent(student)
+                        .then(ServerResponse.ok().build()))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
 
 }
